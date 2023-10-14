0195b98fac0220ccc9f02495f6490f85a7315a2b
MNAth_ <neodarksoulink@gmail.com>
2023-10-14 13:24:19 +0300

    refactor: implement infos middleware into infos module

--- src/infos/infos.middleware.ts
+++ src/infos/infos.middleware.ts
@@ -4,6 +4,8 @@ import { NextFunction, Request, Response } from 'express';
 @Injectable()
 export class InfosMiddleware implements NestMiddleware {
   use(req: Request, res: Response, next: NextFunction) {
+    console.log('Logged here ===>', req.query);
+
     next();
   }
 }
--- src/infos/infos.module.ts
+++ src/infos/infos.module.ts
@@ -1,10 +1,17 @@
-import { Module } from '@nestjs/common';
+import { MiddlewareConsumer, Module, NestModule } from '@nestjs/common';
 import { InfosService } from './infos.service';
 import { InfosController } from './infos.controller';
+
+// MIDDLEWARES
+import { InfosMiddleware } from './infos.middleware';
 
 @Module({
   controllers: [InfosController],
   providers: [InfosService],
   exports: [InfosService],
 })
-export class InfosModule {}
+export class InfosModule implements NestModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer.apply(InfosMiddleware).forRoutes('infos');
+  }
+}
