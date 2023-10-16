53e8cf8328a375fc06ce497d98ced90420cbbbe3
MNAth_ <neodarksoulink@gmail.com>
2023-10-16 18:02:04 +0300

    refactor: log Dto params from infos provider

--- src/infos/infos.service.ts
+++ src/infos/infos.service.ts
@@ -1,10 +1,12 @@
 import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
-// import { CreateInfoDto } from './dto/create-info.dto';
-// import { UpdateInfoDto } from './dto/update-info.dto';
+import { CreateInfoDto } from './dto/create-info.dto';
+import { UpdateInfoDto } from './dto/update-info.dto';
 
 @Injectable()
 export class InfosService {
-  create(/* createInfoDto: CreateInfoDto */) {
+  create(createInfoDto: CreateInfoDto) {
+    console.log(createInfoDto);
+
     return 'This action adds a new info';
   }
 
@@ -16,7 +18,9 @@ export class InfosService {
     return `This action returns a #${id} info`;
   }
 
-  update(id: number /* , updateInfoDto: UpdateInfoDto */) {
+  update(id: number, updateInfoDto: UpdateInfoDto) {
+    console.log(updateInfoDto);
+
     return `This action updates a #${id} info`;
   }
 
