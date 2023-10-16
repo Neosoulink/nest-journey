4b27d9f923fe0d1b86161ab93a4edc52061107a1
MNAth_ <neodarksoulink@gmail.com>
2023-10-16 18:00:22 +0300

    refactor: comment unused params from infos provider

--- src/infos/infos.service.ts
+++ src/infos/infos.service.ts
@@ -1,10 +1,10 @@
 import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
-import { CreateInfoDto } from './dto/create-info.dto';
-import { UpdateInfoDto } from './dto/update-info.dto';
+// import { CreateInfoDto } from './dto/create-info.dto';
+// import { UpdateInfoDto } from './dto/update-info.dto';
 
 @Injectable()
 export class InfosService {
-  create(createInfoDto: CreateInfoDto) {
+  create(/* createInfoDto: CreateInfoDto */) {
     return 'This action adds a new info';
   }
 
@@ -16,7 +16,7 @@ export class InfosService {
     return `This action returns a #${id} info`;
   }
 
-  update(id: number, updateInfoDto: UpdateInfoDto) {
+  update(id: number /* , updateInfoDto: UpdateInfoDto */) {
     return `This action updates a #${id} info`;
   }
 
