07f4257c9b4fac048d93978ffba5513896994002
MNAth_ <neodarksoulink@gmail.com>
2023-10-17 15:35:14 +0300

    refactor: configure `createInfoDto` zod schema

--- src/infos/dto/create-info.dto.ts
+++ src/infos/dto/create-info.dto.ts
@@ -1,1 +1,8 @@
-export class CreateInfoDto {}
+import { z } from 'zod';
+
+export const createInfoSchema = z.object({
+  id: z.number(),
+  name: z.string(),
+});
+
+export type CreateInfoDto = z.infer<typeof createInfoSchema>;
