c108f74fc3244d4e5ac17db12dc3ea438a9a85e0
MNAth_ <neodarksoulink@gmail.com>
2023-10-17 20:53:28 +0300

    refactor: use ZodRawSharp foro ZorObject at the schema DI

--- src/pipes/zodValidation.pipe.ts
+++ src/pipes/zodValidation.pipe.ts
@@ -4,15 +4,15 @@ import {
   Injectable,
   PipeTransform,
 } from '@nestjs/common';
-import { ZodObject } from 'zod';
+import { ZodObject, ZodRawShape } from 'zod';
 
 @Injectable()
 export class ZodValidationPipe implements PipeTransform {
-  constructor(private schema: ZodObject<any>) {}
+  constructor(private schema: ZodObject<ZodRawShape>) {}
 
   transform(value: any /* , metadata: ArgumentMetadata */) {
     try {
-      this.schema.parse(value);
+      return this.schema.parse(value);
     } catch (error) {
       throw new BadRequestException('Validation failed');
     }
