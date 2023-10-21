d3de6565b09c9169e105ebd68052ace65b863b43
MNAth_ <neodarksoulink@gmail.com>
2023-10-21 22:07:37 +0300

    feat: create infos interceptor

---
+++ src/infos/infos.interceptor.spec.ts
@@ -0,0 +1,7 @@
+import { InfosInterceptor } from './infos.interceptor';
+
+describe('InfosInterceptor', () => {
+  it('should be defined', () => {
+    expect(new InfosInterceptor()).toBeDefined();
+  });
+});
