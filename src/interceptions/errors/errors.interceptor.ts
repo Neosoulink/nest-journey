dcd253d2057b0c28c25c0998bbd962d574ada22c
MNAth_ <neodarksoulink@gmail.com>
2023-10-24 01:29:32 +0300

    refactor: implement custom error timeout

--- src/interceptions/errors/errors.interceptor.ts
+++ src/interceptions/errors/errors.interceptor.ts
@@ -4,14 +4,26 @@ import {
   ExecutionContext,
   Injectable,
   NestInterceptor,
+  RequestTimeoutException,
 } from '@nestjs/common';
-import { Observable, catchError, throwError } from 'rxjs';
+import {
+  Observable,
+  TimeoutError,
+  catchError,
+  throwError,
+  timeout,
+} from 'rxjs';
 
 @Injectable()
 export class ErrorsInterceptor implements NestInterceptor {
   intercept(context: ExecutionContext, next: CallHandler): Observable<any> {
     return next.handle().pipe(
+      timeout(5000),
       catchError((err) => {
+        if (err instanceof TimeoutError) {
+          return throwError(() => new RequestTimeoutException());
+        }
+
         return throwError(
           () =>
             new BadRequestException(undefined, {
