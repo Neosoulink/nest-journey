1e4c75ce215fa4a34a0a3cb0b92b7176a85cf1a9
MNAth_ <neodarksoulink@gmail.com>
2023-10-24 01:07:33 +0300

    refactor: remove unused interceptor pipe from errors interceptor

--- src/interceptions/errors/errors.interceptor.ts
+++ src/interceptions/errors/errors.interceptor.ts
@@ -5,26 +5,23 @@ import {
   Injectable,
   NestInterceptor,
 } from '@nestjs/common';
-import { Observable, catchError, map, throwError } from 'rxjs';
+import { Observable, catchError, throwError } from 'rxjs';
 
 @Injectable()
 export class ErrorsInterceptor implements NestInterceptor {
   intercept(context: ExecutionContext, next: CallHandler): Observable<any> {
-    return next
-      .handle()
-      .pipe(
-        catchError((err) => {
-          return throwError(
-            () =>
-              new BadRequestException(undefined, {
-                cause: {
-                  origin: err,
-                  fromInterceptor: true,
-                },
-              }),
-          );
-        }),
-      )
-      .pipe(map((data) => ({ data, date: new Date() })));
+    return next.handle().pipe(
+      catchError((err) => {
+        return throwError(
+          () =>
+            new BadRequestException(undefined, {
+              cause: {
+                origin: err,
+                fromInterceptor: true,
+              },
+            }),
+        );
+      }),
+    );
   }
 }
