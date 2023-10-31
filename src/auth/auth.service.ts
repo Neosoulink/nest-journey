62f02439f8df552f963990e2086c051842e0398f
MNAth_ <neodarksoulink@gmail.com>
2023-10-31 20:17:07 +0300

    refactor: DI of `usersService` in auth controller
    Co-Authored-By: MNAth_ <nathaninbusiness2.0@gmail.com>

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,10 +1,19 @@
 import { Injectable } from '@nestjs/common';
 import { CreateAuthDto } from './dto/create-auth.dto';
 import { UpdateAuthDto } from './dto/update-auth.dto';
+import { UsersService } from '@/users/users.service';
 
 @Injectable()
 export class AuthService {
+  constructor(private usersService: UsersService) {}
+
   create(createAuthDto: CreateAuthDto) {
+    console.log(
+      'Create DTO ==>',
+      createAuthDto,
+      '\n User module service ==>',
+      this.usersService.create.name,
+    );
     return 'This action adds a new auth';
   }
 
@@ -17,6 +26,12 @@ export class AuthService {
   }
 
   update(id: number, updateAuthDto: UpdateAuthDto) {
+    console.log(
+      'Update DTO ==>',
+      updateAuthDto,
+      '\n User module service ==>',
+      this.usersService.update.name,
+    );
     return `This action updates a #${id} auth`;
   }
 
