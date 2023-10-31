9646074a2aa0a5ba1c4b7d60043a76a447cd0935
MNAth_ <neodarksoulink@gmail.com>
2023-10-31 22:13:15 +0300

    feat: setup `ConfigService` provider
    Co-Authored-By: MNAth_ <nathaninbusiness2.0@gmail.com>

--- src/config/config.service.ts
+++ src/config/config.service.ts
@@ -1,4 +1,28 @@
+import { readFileSync } from 'fs';
+import { resolve } from 'path';
 import { Injectable } from '@nestjs/common';
+import * as dotenv from 'dotenv';
+
+// INTERFACES
+import { EnvConfig } from '@/interfaces/env';
 
 @Injectable()
-export class ConfigService {}
+export class ConfigService {
+  private readonly envConfig: EnvConfig;
+
+  constructor() {
+    try {
+      const options = { folder: 'config' };
+      const filePath = `${process.env.NODE_ENV || 'development'}.env`;
+      const envFile = resolve(__dirname, '../../', options.folder, filePath);
+
+      this.envConfig = dotenv.parse(readFileSync(envFile));
+    } catch (error) {
+      console.warn('🚧 Something went wrong ===>', error);
+    }
+  }
+
+  get(key: string): string {
+    return this.envConfig[key];
+  }
+}
